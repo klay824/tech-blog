a4825fb0725ce6d537816a478da60452844bf890
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-10 18:00:27 -0500

    added modules.exports to dashboard routes, rewrote route.use for dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -33,3 +33,5 @@ router.get('/newpost', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const dashboardRoute = require('./dashboardRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoute);
 
 module.exports = router;
