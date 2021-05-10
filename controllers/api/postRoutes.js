ad209de5f3c98c7cf4ae70acc455e13cce098b9f
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-10 17:16:25 -0500

    added redirect to dashboard after creating new post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -9,7 +9,7 @@ router.post('/', withAuth, async (req, res) => {
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newPost);
+    res.redirect('/dashboard');
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,18 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const postData = await Post.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
+    const postData = await Post.destroy(
+      {
+        ...req.body,
+        user_id: req.session.user_id
       },
-    });
-
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-
+      {
+        where: {
+          id: req.params.id
+        }
+      });
     res.status(200).json(postData);
+
   } catch (err) {
     res.status(500).json(err);
   }
