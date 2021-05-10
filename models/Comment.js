6250c449fcc5526c38473621448af1d866ea06bd
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-10 18:06:48 -0500

    commented out timestamp column

--- models/Comment.js
+++ models/Comment.js
@@ -29,11 +29,11 @@ Comment.init(
         key: 'id',
       }
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
+    // date_created: {
+    //   type: DataTypes.DATE,
+    //   allowNull: false,
+    //   defaultValue: DataTypes.NOW,
+    // },
   },
   {
     sequelize,
