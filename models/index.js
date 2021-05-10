74c0fa56ab314102dd35c5949c59d9af4052e0ad
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-10 17:26:08 -0500

    added an onDelete to user hasMany Post

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
