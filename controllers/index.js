83058c82279e536d11e0c6b29ffe97e8130f26c3
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-11 14:42:10 -0500

    removed dashboardroute

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoute = require('./dashboardRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoute);
 
 module.exports = router;
