771e6f1e0dafd79ab4b7b3e50246103c59a59295
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-11 14:27:51 -0500

    rewrote postRoutes more like mini project

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/dashboard', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.redirect('/dashboard');
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -19,14 +19,17 @@ router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy(
       {
-        ...req.body,
-        user_id: req.session.user_id
-      },
-      {
         where: {
-          id: req.params.id
-        }
+          id: req.params.id,
+          user_id: req.session.user_id
+        },
       });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
     res.status(200).json(postData);
 
   } catch (err) {
