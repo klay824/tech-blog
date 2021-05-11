509c3cfcec6ff8a154bf17f05a725d5e0f517d3a
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-11 17:16:56 -0500

    moved delete js to its own file

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -21,26 +21,6 @@ const newFormHandler = async (event) => {
     }
 };
 
-const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
-
-        const response = await fetch(`/api/post/${id}`, {
-            method: 'DELETE',
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Failed to delete project');
-        }
-    }
-};
-
 document
     .querySelector('.new-post-form')
     .addEventListener('submit', newFormHandler);
-
-document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
