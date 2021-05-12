f4ba9f6835ce6c50317d21494fc90f62359166c7
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-11 22:13:35 -0500

    wrote create post js and added link to new entry

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,26 +1,24 @@
-const newFormHandler = async (event) => {
+const createPost = async (event) => {
     event.preventDefault();
 
-    const title = document.querySelector('#post-title').value.trim();
-    const body = document.querySelector('#post-desc').value.trim();
+    const title = document.querySelector('#blog-title').value.trim();
+    const body = document.querySelector('#blog-content').value.trim();
 
-    if (title && body) {
-        const response = await fetch(`/api/posts/dashboard`, {
-            method: 'POST',
-            body: JSON.stringify({ title, body }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Failed to create project');
-        }
+    const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({ title, body }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to create project');
     }
+
 };
 
-document
-    .querySelector('.new-post-form')
-    .addEventListener('submit', newFormHandler);
+document.querySelector('.new-post-form').addEventListener('submit', createPost);
