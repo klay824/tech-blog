3d157e322ffca9c812e68e6581b6736e3830edda
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-11 20:02:50 -0500

    removed dashboard

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/dashboard', withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
@@ -12,6 +12,32 @@ router.post('/dashboard', withAuth, async (req, res) => {
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        body: req.body.content,
+      },
+      {
+        where: {
+          id: req.params, id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
